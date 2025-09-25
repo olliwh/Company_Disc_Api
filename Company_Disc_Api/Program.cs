961f3f48219549d579cdd2427a99b744cf2e31dc
ollwh <98967791+olliwh@users.noreply.github.com>
2025-09-25 13:23:59 +0200

    added docker fil to be deployed on render.com

--- Company_Disc_Api/Program.cs
+++ Company_Disc_Api/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
 });
 
 // Add services to the container.
-
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -55,8 +55,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ApiKeyMiddleware>();
 
-
+app.UseAuthorization();
 app.MapControllers();
+
+
 
 app.Run();
 
